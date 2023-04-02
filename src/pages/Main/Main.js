1ce2172d6e334b8913036be69ccb40ed54f5dc03
18137036 <9wjdgh9@naver.com>
2023-04-02 20:14:01 +0900

    :lipstick: [Design] 메인페이지 초기 스타일 작업

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,29 @@
 import React from "react";
-
+import styles from "./Main.module.css";
 const Main = () => {
   return (
     <>
-      <h1>메인</h1>
+      {/* 텍스트 영역 */}
+      <div className={styles.text_area}>
+        <div className={styles.title}>Portfolio</div>
+        <h1>
+          <div className={styles.name}>
+            LEE
+            <div className={styles.line}></div>
+            <br />
+            JUNG HO
+          </div>
+        </h1>
+        <p className={styles.introduce}>Hello, I'm Junior FrontEnd Developer</p>
+      </div>
+      {/* 원 영역 */}
+      <div className={styles.circle_wrapper}>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+      </div>
+      {/* 뱃지 영역 */}
+      <div className={styles.badge}>Home</div>
     </>
   );
 };

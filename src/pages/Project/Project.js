68bc4b3175d16a79189a5962d8b8179508623aee
18137036 <9wjdgh9@naver.com>
2023-04-03 23:11:01 +0900

    :lipstick: [Design] 프로젝트 페이지 스타일 작업 완료

---
+++ src/pages/Project/Project.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styles from "./Project.module.css";
+
+const Project = () => {
+  return (
+    <>
+      <div className={styles.featured}>
+        <div className={styles.count}>8</div>
+        <div className={styles.text_area}>
+          <h1 className={styles.title}>
+            Project <sup>1</sup>
+          </h1>
+          <p className={styles.introduce}>지금까지 작업한 프로젝트</p>
+        </div>
+      </div>
+
+      <ul className={styles.swiper}>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <ol className={styles.list}>
+        <li className={styles.item}>
+          <div className={styles.num}>1</div>
+          <div className={styles.infos}>
+            <div className={styles.title}>Photoday</div>
+            <div className={styles.desc}>Lorem Ipsum is simply dummy</div>
+          </div>
+        </li>
+
+        <li className={styles.item}>
+          <div className={styles.num}>2</div>
+          <div className={styles.infos}>
+            <div className={styles.title}>Damda</div>
+            <div className={styles.desc}>Lorem Ipsum is simply dummy</div>
+          </div>
+        </li>
+
+        <li className={styles.item}>
+          <div className={styles.num}>3</div>
+          <div className={styles.infos}>
+            <div className={styles.title}>MungDoList</div>
+            <div className={styles.desc}>Lorem Ipsum is simply dummy</div>
+          </div>
+        </li>
+      </ol>
+    </>
+  );
+};
+
+export default Project;

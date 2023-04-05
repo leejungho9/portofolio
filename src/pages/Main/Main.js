ed279534632afef449c8206cfd2a1793cfb61c75
18137036 <9wjdgh9@naver.com>
2023-04-05 12:53:09 +0900

    :lipstick: [Design] Main className 변경

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,20 +1,25 @@
 import React from "react";
 import styles from "./Main.module.css";
+import classNames from "classnames/bind";
+const cn = classNames.bind(styles);
+
 const Main = () => {
   return (
-    <>
-      {/* 텍스트 영역 */}
-      <div className={styles.text_area}>
-        <div className={styles.title}>Portfolio</div>
+    <section className="content">
+      <div className={cn("text_area", "stagger-wrapper")}>
+        <div className={cn("title", "stagger-item")}>Portfolio</div>
         <h1>
           <div className={styles.name}>
-            LEE
-            <div className={styles.line}></div>
-            <br />
-            JUNG HO
+            <div className={cn("stagger-item")}>
+              LEE
+              <div className={styles.line}></div>
+            </div>
+            <div className={cn("stagger-item")}>JUNG HO</div>
           </div>
         </h1>
-        <p className={styles.introduce}>Hello, I'm Junior FrontEnd Developer</p>
+        <p className={cn("stagger-item", "introduce")}>
+          Hello, I'm FrontEnd Developer
+        </p>
       </div>
       {/* 원 영역 */}
       <div className={styles.circle_wrapper}>
@@ -23,8 +28,8 @@ const Main = () => {
         <div className={styles.circle}></div>
       </div>
       {/* 뱃지 영역 */}
-      <div className={styles.badge}>Home</div>
-    </>
+      {/* <div className={styles.badge}>Home</div> */}
+    </section>
   );
 };
 

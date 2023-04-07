d13e0611ea8ab80b12f52570c9d159df5ccdd2b1
18137036 <9wjdgh9@naver.com>
2023-04-07 15:56:56 +0900

    :lipstick: [Design] Components 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
+import Introduce from "./pages/Introduce/Introduce";
 import Main from "./pages/Main/Main";
+import Project from "./pages/Project/Project";
 
 function App() {
-  return <Main />;
+  return (
+    <>
+      <Main />
+      <Introduce />
+      <Project />
+    </>
+  );
 }
 
 export default App;

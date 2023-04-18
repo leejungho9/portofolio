526333f7a7d2ff08cf418042c7ffdd56c81d93be
leejungho9 <9wjdgh9@naver.com>
2023-04-18 19:26:15 +0900

    :hammer: [Update] a 태그 href 연결 및 Introduce 수정

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -31,28 +31,49 @@ const Introduce = () => {
               <div className={cn("desc")}>
                 안녕하세요! 저는 하고 싶은 일이나 궁금한 것이 생기면 직접 부딪혀
                 보고, 제 힘으로 알아내는 것을 좋아하는 프론트엔드 개발자
-                이정호입니다. 단순히 웹 디자인에서 그치지 않고 이를 직접 코드를
-                작성하고 결과물이 실제 브라우저에서 동작하는 것을 보며 크고 작은
-                성취감과 희열감을 느끼며 웹 개발에 더욱 빠져들게 되었습니다.
-                아직은 많이 부족하지만, 변화하는 세상에 맞춰 자기 개발을 미루지
-                않고, 기본기 탄탄한 웹 프론트엔드 개발자가 되기 위해
-                노력하겠습니다.
+                이정호입니다. 고등학생 때부터 디자인 툴을 사용하며 디자인 관련
+                일만 하던 중, 단순히 디자인에서 그치지 않고 이를 직접 구현하고
+                싶다는 생각에 무작정 강의를 찾아보면서 어설프게 만들어본
+                사이트가 브라우저에서 동작하는 것을 보며 크고 작은 성취감과
+                희열감을 느꼈고, 이를 계기로 웹 개발에 빠져들게 되었습니다.
+                현재는 생산성을 높이기 위한 컴포넌트와 디자인 시스템에 대한
+                고민을 하며, 사용자 경험을 고려한 UI/UX 개발에 관심이 많습니다.
               </div>
             </div>
 
             <div className={cn("info")}>
-              <div className={cn("email", "info_item", "mouse_effect")}>
-                <a href="mailto:oje12345671@gamil.com">Email</a>
+              <div className={cn("email", "info_item")}>
+                <a
+                  href="mailto:oje12345671@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Email
+                </a>
               </div>
 
-              <div className={cn("email", "info_item", "mouse_effect")}>
-                <a href="mailto:oje12345671@gamil.com">Github</a>
+              <div className={cn("email", "info_item")}>
+                <a href="https://github.com/" target="_blank" rel="noreferrer">
+                  Github
+                </a>
               </div>
-              <div className={cn("email", "info_item", "mouse_effect")}>
-                <a href="mailto:oje12345671@gamil.com">Blog</a>
+              <div className={cn("email", "info_item")}>
+                <a
+                  href="https://velog.io/@leejungho9"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Blog
+                </a>
               </div>
-              <div className={cn("email", "info_item", "mouse_effect")}>
-                <a href="mailto:oje12345671@gamil.com">Resume</a>
+              <div className={cn("email", "info_item")}>
+                <a
+                  href="https://adorable-viscount-bb5.notion.site/LEE-JUNG-HO-545433e4bce547bdaf89dd78b8d73220"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Resume
+                </a>
               </div>
             </div>
           </div>
@@ -78,10 +99,9 @@ const Introduce = () => {
                     </td>
                     <td className={cn("table_cell")}>REACT</td>
                     <td className={cn("table_cell")}>
-                      필요한 컴포넌트를 구현할 수 있고 Hooks를 통해 상태 값을
-                      생성하고 관리할 수 있습니다. Styled Components
-                      라이브러리를 통해 컴포넌트들을 스타일링 할 수 있습니다.
-                      Redux를 활용한 효율적인 상태 관리를 추구합니다.
+                      JSX 문법과 컴포넌트 기반 아키텍처를 충분히 이해하고
+                      활용하고 불필요한 렌더링을 최소화하기 컴포넌트를 작은
+                      단위로 분할하여 작업할 수 있습니다.
                     </td>
                   </tr>
                   <tr>
@@ -94,8 +114,10 @@ const Introduce = () => {
                     </td>
                     <td className={cn("table_cell")}>JAVASCRIPT</td>
                     <td className={cn("table_cell")}>
-                      ES2015 이전과 이후 모두 자바스크립트 문법을 알고 DOM을
-                      조작하는 것에 어려움이 없습니다.
+                      Promise와 async/await를 이용하여 비동기 처리를 할 수
+                      있습니다. 또한, DOM 구조를 파악하여 필요한 요소를 선택하고
+                      조작하여 동적인 UI를 구현하거나, 이벤트 핸들링 등을 처리할
+                      수 있습니다.
                     </td>
                   </tr>
                   <tr>
@@ -108,8 +130,8 @@ const Introduce = () => {
                     </td>
                     <td className={cn("table_cell")}>TYPESCRIPT</td>
                     <td className={cn("table_cell")}>
-                      TypeScript를 React의 State나 변수, 함수 등을 컴파일 타임에
-                      체크하여 협업의 효율과 코드 개발의 생산성을 높일 수
+                      정적 타입 검사를 수행하고 인터페이스를 사용하여 코드
+                      재사용성을 높이면 코드 안정성과 유지 보수성을 향상시킬 수
                       있습니다.
                     </td>
                   </tr>
@@ -139,8 +161,8 @@ const Introduce = () => {
                     </td>
                     <td className={cn("table_cell")}>CSS3</td>
                     <td className={cn("table_cell")}>
-                      Emotion을 활용한 CSS-in-JS 방식으로 스타일링 작업이
-                      가능합니다. Media Query를 이용해 반응형 웹 개발이
+                      styled-components을 활용한 CSS-in-JS 방식으로 스타일링
+                      작업이 가능합니다. Media Query를 이용해 반응형 웹 개발이
                       가능합니다.
                     </td>
                   </tr>
@@ -154,9 +176,9 @@ const Introduce = () => {
                     </td>
                     <td className={cn("table_cell")}>GIT</td>
                     <td className={cn("table_cell")}>
-                      Git을 통해 로컬 및 리모트 저장소를 나누어 작업할 수
-                      있습니다. 브랜치를 main 브랜치와 develop 브랜치로 나누어
-                      효율적인 방식으로 코드 및 커밋을 관리합니다.
+                      Git을 이용하여 버전 관리를 하고, 브랜치 기능을 활용하여
+                      코드를 안정적으로 관리할 수 있어 효율적인 협업과 유지
+                      보수가 가능합니다.
                     </td>
                   </tr>
                 </table>
